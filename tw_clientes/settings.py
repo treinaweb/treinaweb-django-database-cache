07795da7967d6772113b1dbed17b1e30a97c309c
fagnerpsantos <fagnerpinheirosantos@gmail.com>
2020-09-28 19:03:33 -0300

    03_Criando_Tabela_Cache

--- tw_clientes/settings.py
+++ tw_clientes/settings.py
@@ -86,6 +86,13 @@ DATABASES = {
     }
 }
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.db.DatabaseCache',
+        'LOCATION': 'tabela_cache_dois',
+    }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
